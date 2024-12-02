d8c46401499ddc195ade90e919adb4bee81c7ea0
tatsumakiroll <paul.shoreman@outlook.com>
2024-12-02 18:55:03 +0000

    started tab navigation for the expo app

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,36 @@
+import { Tabs } from "expo-router";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: "blue" }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="home" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: "Account",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="user" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="cog" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet, Image, Text, View} from "react-native";
+
+export default function Tab() {
+  return (
+    <View>
+      <Text>[Settings]</Text>
+    </View>
+  );
+}
